0d104dd6dccc5dd9a0ef96b8c26480d93f36b7ac
Jeni Gamracy <github@jenigamracy.ca>
2024-05-07 06:24:27 -0600

    Add samples for tuples

---
+++ data_structures.py
@@ -0,0 +1,23 @@
+# Learning tuples!
+
+# https://docs.python.org/3/tutorial/datastructures.html
+
+###############################################################################
+# TUPLES
+
+# Tuples are immutable (ie. an object with (a) fixed value(s), so they cannot
+# be altered)
+
+a = 1234, 4312, "hello"
+
+# On output, tuples are always in parentheses
+print(a)
+
+###############################################################################
+
+# Nested tuples
+
+b = a, 1, 2, 3, 4, 5
+
+print(b)
+
