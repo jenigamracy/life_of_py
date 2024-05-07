01500d8f4d7d148fe215ef8c494537ebb7baf9f7
Jeni Gamracy <github@jenigamracy.ca>
2024-05-07 10:55:30 -0600

    Add notes and provide example of immutable tuple

--- data_structures.py
+++ data_structures.py
@@ -2,22 +2,65 @@
 
 # https://docs.python.org/3/tutorial/datastructures.html
 
+def add_break():
+    print("\n====================\n")
+
 ###############################################################################
 # TUPLES
 
-# Tuples are immutable (ie. an object with (a) fixed value(s), so they cannot
-# be altered)
+# A tuple is sequence data type (others being lists and strings)
+
+########################
+# Example: Basic tuple 
+########################
+
+print("BASIC TUPLE\n")
 
 a = 1234, 4312, "hello"
+print(a) # On output, tuples are always in parentheses
 
-# On output, tuples are always in parentheses
-print(a)
+add_break()
 
-###############################################################################
+#########################
+# Example: Nested tuples
+#########################
 
-# Nested tuples
+print("NESTED TUPLE\n")
 
 b = a, 1, 2, 3, 4, 5
-
 print(b)
+
+add_break()
+
+
+#####################################
+# Did you know? Tuples are immutable
+#####################################
+
+print("TUPLES ARE IMMUTABLE\n")
+
+c = 3, 2, 1, 8, 9
+# c[0] = 2
+# Will result in an error
+#   "TypeError: 'tuple' object does not support item assignment"
+# This is because tuples are immutable, meaning the values are fixed and 
+# cannot be altered
+
+# However, you can add mutable objects to tuples
+print("BUT YOU CAN HAVE MUTABLE OBJECTS IN TUPLES\n")
+
+d = [9, 8, 7, 6]
+print("array d:", d, sep=" ", end="\n\n")
+
+e = d, (1, 2, 3)
+print("tuple e with d as first element:", e, sep="", end="\n\n")
+
+d[0] = 1
+print("d and e again after changing first element of d\n")
+print("d:", d, sep=" ")
+print("e:", e, sep=" ")
+add_break()
+
+
+###############################################################################
 
